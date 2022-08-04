ab6956f8c4cbd10b2d89f6bf3848cc98a23b8e80
Shlok Mohanty <59838234+ShlokMohanty@users.noreply.github.com>
2022-08-04 18:06:47 +0530

    day_1

---
+++ watermelon_codeforces_div_A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int W ;
+    cin>>W;
+    if(W%2==0)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+       cout<<"No"<<endl; 
+    }
+    
+}
